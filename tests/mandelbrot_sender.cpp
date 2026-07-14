673e147e036b350ccf9443b06f6a4806dd4d4acd
Alexandr <asbelikov1@gmail.com>
2026-07-14 14:25:28 +0000

    Tests mocked

--- tests/mandelbrot_sender.cpp
+++ tests/mandelbrot_sender.cpp
@@ -1,20 +1,17 @@
 #include "mandelbrot_sender.hpp"
 #include "types_core.hpp"
 #include "types_sfml.hpp"
+#include "gmock/gmock.h"
 #include "gtest/gtest.h"
-// #include "gmock/gmock.h"
 #include <SFML/Config.hpp>
 #include <stdexec/__detail/__execution_fwd.hpp>
 
 namespace ex = stdexec;
 
-// struct MockCounter {
-//     MOCK_METHOD(void, value_set, (), ());
-//     MOCK_METHOD(void, error_set, (), ());
-//     MOCK_METHOD(void, stopped_set, (), ());
-// };
-
-using MockCounter = int;
+struct MockCounter {
+    MOCK_METHOD(void, value_set, (), ());
+    MOCK_METHOD(void, error_set, (), ());
+};
 
 struct MockReceiver {
     using receiver_concept = ex::receiver_t;
@@ -22,23 +19,24 @@ struct MockReceiver {
 
     MockReceiver(MockCounter &mc) : mock_counter(mc) {}
 
-    void set_value(FrameBuffer *fb) noexcept { mock_counter++; }
+    void set_value(FrameBuffer *fb) noexcept { mock_counter.value_set(); }
 
     template <typename Error>
     void set_error(Error &&error) noexcept {
-        mock_counter++;
+        mock_counter.error_set();
     }
 };
 
 TEST(MandelbrotSenderTest, ReceiverTest) {
-    RenderSettings settings{.width = 2, .height = 2, .max_iterations = 200, .escape_radius = 4.0};
+    RenderSettings settings{.width = 800, .height = 600, .max_iterations = 200, .escape_radius = 4.0};
     ViewPort viewport = AppState::INITIAL_VIEWPORT;
     auto sender = mandelbrot::MakeComputeSender(settings, viewport);
     MockCounter mc{};
     FrameBuffer fb = FrameBuffer::Make(settings.width, settings.height);
     auto state = ex::connect(ex::just(&fb) | std::move(sender), MockReceiver{mc});
+    EXPECT_CALL(mc, value_set).Times(1);
+    EXPECT_CALL(mc, error_set).Times(0);
     ex::start(state);
-    EXPECT_EQ(mc, 1);
 }
 
 TEST(MandelbrotSenderTest, ResultTest) {
@@ -48,6 +46,10 @@ TEST(MandelbrotSenderTest, ResultTest) {
     FrameBuffer fb = FrameBuffer::Make(settings.width, settings.height);
     auto result = ex::sync_wait(ex::just(&fb) | std::move(sender));
     ASSERT_TRUE(result.has_value());
-    EXPECT_EQ(std::get<0>(result.value()), &fb);
-    EXPECT_EQ(fb.rgba, std::vector<sf::Uint8>({0xFF, 0xF, 0x0, 0xFF, 0xFF, 0xF, 0x0, 0xFF, 0xFF, 0x16, 0x0, 0xFF, 0x0, 0x0, 0x0, 0xFF}));
+    auto& fb_result = std::get<0>(result.value());
+    EXPECT_EQ(fb_result, &fb);
+    EXPECT_EQ(fb_result->height, 2);
+    EXPECT_EQ(fb_result->width, 2);
+    EXPECT_EQ(fb_result->rgba, std::vector<sf::Uint8>(
+                           {0xFF, 0xF, 0x0, 0xFF, 0xFF, 0xF, 0x0, 0xFF, 0xFF, 0x16, 0x0, 0xFF, 0x0, 0x0, 0x0, 0xFF}));
 }
